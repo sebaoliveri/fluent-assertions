fd0357e81908f74745c6c6f453efed345df96bd3
Sebastian Oliveri <sebastianoliveri@Sebastians-MacBook-Pro.local>
2020-01-14 11:21:39 -0300

    made arg non strict for flat map

--- src/main/scala/expression/FlatMapAssertionExp.scala
+++ src/main/scala/expression/FlatMapAssertionExp.scala
@@ -4,7 +4,7 @@ case class FlatMapAssertionExp[T](left: LogicalOperatorsExp[T,AssertionResultBeh
                                   right: LogicalOperatorsExp[T,AssertionResultBehaviour[T]])
   extends LogicalOperatorsExp[T,AssertionResultBehaviour[T]] {
 
-  def followedBy(assertionExp: LogicalOperatorsExp[T,AssertionResultBehaviour[T]]): FlatMapAssertionExp[T] =
+  def followedBy(assertionExp: => LogicalOperatorsExp[T,AssertionResultBehaviour[T]]): FlatMapAssertionExp[T] =
     FlatMapAssertionExp(left = this, right = assertionExp)
 
   override def evaluate(context: T): AssertionResultBehaviour[T] =
